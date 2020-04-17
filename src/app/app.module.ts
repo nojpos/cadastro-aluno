ab39dbdb5c7785dd49a8d8578ff181b9880761f8
Joao Paulo <joaopaulloojpos15@gmail.com>
2020-04-16 21:50:55 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AlunoComponent } from './cadastro/aluno/aluno.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AlunoComponent
   ],
   imports: [
     BrowserModule
